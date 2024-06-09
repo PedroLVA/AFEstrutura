2b1296fd1bb67ae04083b022812a488f8829cc25
Pedro Athayde <pedro_athayde@outlook.com>
2024-06-09 12:51:35 -0300

    fixed cards

--- src/app/components/ver-produtos/ver-produtos.component.ts
+++ src/app/components/ver-produtos/ver-produtos.component.ts
@@ -14,7 +14,7 @@ export class VerProdutosComponent {
 
   constructor() {
     this.products = [
-      { name: 'Produto 1', description: 'Descrição do produto 1', price: 10.00 },
+      { name: 'Produto 1', description: 'Descrição do produto 1Descrição do produto 1Descrição do produto 1Descrição do produto 1Descrição do produto 1Descrição do produto 1Descrição do produto 1Descrição do produto 1Descrição do produto 1', price: 10.00 },
       { name: 'Produto 2', description: 'Descrição do produto 2', price: 20.00 },
       { name: 'Produto 3', description: 'Descrição do produto 3', price: 30.00 }
     ];
